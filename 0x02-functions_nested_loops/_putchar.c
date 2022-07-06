2943f0b37314b6649864cef9c0f8e97a2998bd9f
Beljat <jajabethel76@gmail.com>
2022-07-06 05:57:27 -0700

    0-putchar

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main - Entry point
- *
+ * 
  * Return always 0(Success)
  */
 int main(void)
@@ -9,7 +9,7 @@ int main(void)
 	char str[] = "_putchar";
 	int ch;
 
-	for (ch = 0; ch < 8; + + ch)
+	for (ch = 0; ch < 8; ++ ch)
 		_putchar(str[ch]);
 	_putchar('\n');
 
