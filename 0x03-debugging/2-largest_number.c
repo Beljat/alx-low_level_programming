e63aa0d852009c054717009e87db54e3185f348a
Beljat <jajabethel76@gmail.com>
2022-07-07 18:06:28 -0700

    sorry git

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * * largest_number - returns the largest of 3 numbers
- * * @a: first integer
- * * @b: second integer
- * * @c: third integer
- * * Return: largest number
+ *  * * largest_number - returns the largest of 3 numbers
+ *   * * @a: first integer
+ *    * * @b: second integer
+ *     * * @c: third integer
+ *      * * Return: largest number
  */
 
 int largest_number(int a, int b, int c)
@@ -25,6 +25,5 @@ int largest_number(int a, int b, int c)
 		largest = c;
 	}
 
-	return (larest);
+	return (largest);
 }
-
